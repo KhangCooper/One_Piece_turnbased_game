fd01545e85895db7c32fe2716cce1fbdf0c2ea3f
KhangCooper <113864410+KhangCooper@users.noreply.github.com>
2026-05-24 00:17:06 +0700

    Update the main function to show result

--- main.cpp
+++ main.cpp
@@ -4,5 +4,18 @@
 using namespace std;
 
 int main(int argc, const char * argv[]) {
+    string filename = "sample_input";
+    cout << ">>>>>>>> RUNNING <<<<<<<<" << endl;
+    cout << "--- INITIALIZE THE BATTLE ---" << endl;
+    
+    EniesLobbyBattle battle(filename);
+    cout << "--- PROCESSING ---" << endl;
+    
+    battle.runBattle();
+    string result = battle.getResult();
+    
+    cout << "--- FINAL RESULT ---" << endl;
+    cout << result << endl;
+    
     return 0;
 }
